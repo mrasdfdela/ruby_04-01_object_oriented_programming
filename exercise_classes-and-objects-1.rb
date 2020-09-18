ce7602f06f7e670dc4675dc2d6235928642ea7e5
mrasdfdela <jerryhsu830@gmail.com>
2020-09-18 12:32:31 -0400

    exercise: inheritance, pt. 2

--- exercise_classes-and-objects-1.rb
+++ exercise_classes-and-objects-1.rb
@@ -1,39 +1,14 @@
 class Vehicle
     attr_reader :year, :model
-    attr_accessor :color
+    attr_accessor :color, :speed
+    @@garage = 0
     
     def initialize(year, color, model)
         @year = year
         @color = color
         @model = model
-    end
-
-    def to_s 
-        "Your #{@year} #{@color} #{self.model} looks wonderful!"
-    end    attr_accessor :speed
-
-    def self.gas_mileage(miles, gallon)
-        gas_mileage = miles / gallon
-        return gas_mileage
-    end
-end
-
-class MyCar < Vehicle    
-    NUMBER_OF_DOORS = 4
-    attr_accessor :speed
-    def initialize(speed)
         @speed = 0
-    end
-
-    def spray_paint(new_color)
-        @color = new_color
-    end
-    def to_s 
-        "Your #{@year} #{@color} #{self.model} looks wonderful!"
-    end    attr_accessor :speed
-
-    def initialize(speed)
-        @speed = 0
+        @@garage += 1
     end
 
     def speed_up
@@ -42,23 +17,37 @@ class MyCar < Vehicle
     def brake
         self.speed > 0 ? self.speed -= 1 : self.speed = 0
     end
-
     def shut_off
         self.speed = 0
     end
 
+    def spray_paint(new_color)
+        @color = new_color
+    end
+    def to_s 
+        "Your #{@year} #{@color} #{self.model} looks wonderful!"
+    end
+
+    def self.gas_mileage(miles, gallon)
+        gas_mileage = miles / gallon
+        return gas_mileage
+    end
+    def self.garage
+        @@garage
+    end
+end
+
+class MyCar < Vehicle    
+    NUMBER_OF_DOORS = 4
+    def initialize(year, color, model)
+        super(year, color, model)
+    end
 end
 
 class MyTruck < Vehicle
     NUMBER_OF_DOORS = 2
-    def initialize(grille_guard)
-        @grille_guard = false
-    end
-    def install_guard
-        @grille_guard = true
-    end
-    def remove_guard
-        @grille_guard = false
+    def initialize(year, color, model)
+        super(year, color, model)
     end
 end
 
@@ -80,3 +69,7 @@ jerry.spray_paint("white")
 puts jerry
 
 puts MyCar.gas_mileage(34,1)
+
+cybertruck = MyTruck.new(2021, "silver", "cybertruck")
+
+puts Vehicle.garage
